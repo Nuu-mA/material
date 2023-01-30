381acd85a543841b3b502404d9ca01897007a8ae
NuumA <numa.phone.g@gmail.com>
2023-01-30 23:12:27 +0900

    ViewModel層の実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:material/view/templates/material_main/widget.dart';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
