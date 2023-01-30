c5bb2af1013e0e81095d42d10b31edfec0f2ea22
NuumA <numa.phone.g@gmail.com>
2023-01-30 23:11:48 +0900

    WaterEntityに変更

---
+++ lib/domain/entity/water/entity.dart
@@ -0,0 +1,10 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'entity.freezed.dart';
+
+@freezed
+class WaterEntity with _$WaterState {
+  const factory WaterEntity.solid() = _WaterSolid;
+  const factory WaterEntity.liquid() = _WaterLiquid;
+  const factory WaterEntity.gas() = _WaterGas;
+}
---
+++ lib/domain/entity/water/entity.freezed.dart
@@ -0,0 +1,405 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'entity.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$WaterState {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() solid,
+    required TResult Function() liquid,
+    required TResult Function() gas,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? solid,
+    TResult? Function()? liquid,
+    TResult? Function()? gas,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? solid,
+    TResult Function()? liquid,
+    TResult Function()? gas,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_WaterSolid value) solid,
+    required TResult Function(_WaterLiquid value) liquid,
+    required TResult Function(_WaterGas value) gas,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_WaterSolid value)? solid,
+    TResult? Function(_WaterLiquid value)? liquid,
+    TResult? Function(_WaterGas value)? gas,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_WaterSolid value)? solid,
+    TResult Function(_WaterLiquid value)? liquid,
+    TResult Function(_WaterGas value)? gas,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $WaterStateCopyWith<$Res> {
+  factory $WaterStateCopyWith(
+          WaterEntity value, $Res Function(WaterEntity) then) =
+      _$WaterStateCopyWithImpl<$Res, WaterEntity>;
+}
+
+/// @nodoc
+class _$WaterStateCopyWithImpl<$Res, $Val extends WaterEntity>
+    implements $WaterStateCopyWith<$Res> {
+  _$WaterStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+}
+
+/// @nodoc
+abstract class _$$_WaterSolidCopyWith<$Res> {
+  factory _$$_WaterSolidCopyWith(
+          _$_WaterSolid value, $Res Function(_$_WaterSolid) then) =
+      __$$_WaterSolidCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$_WaterSolidCopyWithImpl<$Res>
+    extends _$WaterStateCopyWithImpl<$Res, _$_WaterSolid>
+    implements _$$_WaterSolidCopyWith<$Res> {
+  __$$_WaterSolidCopyWithImpl(
+      _$_WaterSolid _value, $Res Function(_$_WaterSolid) _then)
+      : super(_value, _then);
+}
+
+/// @nodoc
+
+class _$_WaterSolid implements _WaterSolid {
+  const _$_WaterSolid();
+
+  @override
+  String toString() {
+    return 'WaterState.solid()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$_WaterSolid);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() solid,
+    required TResult Function() liquid,
+    required TResult Function() gas,
+  }) {
+    return solid();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? solid,
+    TResult? Function()? liquid,
+    TResult? Function()? gas,
+  }) {
+    return solid?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? solid,
+    TResult Function()? liquid,
+    TResult Function()? gas,
+    required TResult orElse(),
+  }) {
+    if (solid != null) {
+      return solid();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_WaterSolid value) solid,
+    required TResult Function(_WaterLiquid value) liquid,
+    required TResult Function(_WaterGas value) gas,
+  }) {
+    return solid(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_WaterSolid value)? solid,
+    TResult? Function(_WaterLiquid value)? liquid,
+    TResult? Function(_WaterGas value)? gas,
+  }) {
+    return solid?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_WaterSolid value)? solid,
+    TResult Function(_WaterLiquid value)? liquid,
+    TResult Function(_WaterGas value)? gas,
+    required TResult orElse(),
+  }) {
+    if (solid != null) {
+      return solid(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _WaterSolid implements WaterEntity {
+  const factory _WaterSolid() = _$_WaterSolid;
+}
+
+/// @nodoc
+abstract class _$$_WaterLiquidCopyWith<$Res> {
+  factory _$$_WaterLiquidCopyWith(
+          _$_WaterLiquid value, $Res Function(_$_WaterLiquid) then) =
+      __$$_WaterLiquidCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$_WaterLiquidCopyWithImpl<$Res>
+    extends _$WaterStateCopyWithImpl<$Res, _$_WaterLiquid>
+    implements _$$_WaterLiquidCopyWith<$Res> {
+  __$$_WaterLiquidCopyWithImpl(
+      _$_WaterLiquid _value, $Res Function(_$_WaterLiquid) _then)
+      : super(_value, _then);
+}
+
+/// @nodoc
+
+class _$_WaterLiquid implements _WaterLiquid {
+  const _$_WaterLiquid();
+
+  @override
+  String toString() {
+    return 'WaterState.liquid()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$_WaterLiquid);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() solid,
+    required TResult Function() liquid,
+    required TResult Function() gas,
+  }) {
+    return liquid();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? solid,
+    TResult? Function()? liquid,
+    TResult? Function()? gas,
+  }) {
+    return liquid?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? solid,
+    TResult Function()? liquid,
+    TResult Function()? gas,
+    required TResult orElse(),
+  }) {
+    if (liquid != null) {
+      return liquid();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_WaterSolid value) solid,
+    required TResult Function(_WaterLiquid value) liquid,
+    required TResult Function(_WaterGas value) gas,
+  }) {
+    return liquid(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_WaterSolid value)? solid,
+    TResult? Function(_WaterLiquid value)? liquid,
+    TResult? Function(_WaterGas value)? gas,
+  }) {
+    return liquid?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_WaterSolid value)? solid,
+    TResult Function(_WaterLiquid value)? liquid,
+    TResult Function(_WaterGas value)? gas,
+    required TResult orElse(),
+  }) {
+    if (liquid != null) {
+      return liquid(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _WaterLiquid implements WaterEntity {
+  const factory _WaterLiquid() = _$_WaterLiquid;
+}
+
+/// @nodoc
+abstract class _$$_WaterGasCopyWith<$Res> {
+  factory _$$_WaterGasCopyWith(
+          _$_WaterGas value, $Res Function(_$_WaterGas) then) =
+      __$$_WaterGasCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$_WaterGasCopyWithImpl<$Res>
+    extends _$WaterStateCopyWithImpl<$Res, _$_WaterGas>
+    implements _$$_WaterGasCopyWith<$Res> {
+  __$$_WaterGasCopyWithImpl(
+      _$_WaterGas _value, $Res Function(_$_WaterGas) _then)
+      : super(_value, _then);
+}
+
+/// @nodoc
+
+class _$_WaterGas implements _WaterGas {
+  const _$_WaterGas();
+
+  @override
+  String toString() {
+    return 'WaterState.gas()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$_WaterGas);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() solid,
+    required TResult Function() liquid,
+    required TResult Function() gas,
+  }) {
+    return gas();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? solid,
+    TResult? Function()? liquid,
+    TResult? Function()? gas,
+  }) {
+    return gas?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? solid,
+    TResult Function()? liquid,
+    TResult Function()? gas,
+    required TResult orElse(),
+  }) {
+    if (gas != null) {
+      return gas();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_WaterSolid value) solid,
+    required TResult Function(_WaterLiquid value) liquid,
+    required TResult Function(_WaterGas value) gas,
+  }) {
+    return gas(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_WaterSolid value)? solid,
+    TResult? Function(_WaterLiquid value)? liquid,
+    TResult? Function(_WaterGas value)? gas,
+  }) {
+    return gas?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_WaterSolid value)? solid,
+    TResult Function(_WaterLiquid value)? liquid,
+    TResult Function(_WaterGas value)? gas,
+    required TResult orElse(),
+  }) {
+    if (gas != null) {
+      return gas(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _WaterGas implements WaterEntity {
+  const factory _WaterGas() = _$_WaterGas;
+}
