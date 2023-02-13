ef9ae6017e4ae39cc29132d2239bdb70d311bf23
NuumA <numa.phone.g@gmail.com>
2023-02-13 11:42:09 +0900

    TemperatureEntityの削除

--- lib/domain/entity/temperature/entity.dart
+++
@@ -1,9 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'entity.freezed.dart';
-
-@freezed
-class TemperatureEntity with _$TemperatureEntity {
-  const factory TemperatureEntity({required final int temperature}) =
-      _TemperatureState;
-}
